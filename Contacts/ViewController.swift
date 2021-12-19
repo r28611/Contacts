3040d2bb4ffc6f5574dfab589b8ced249d445b70
Margarita Novokh <r28611@gmail.com>
2021-12-19 22:45:32 +0100

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Margarita Novokhatskaia on 19/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
