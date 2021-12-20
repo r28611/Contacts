e81136bb721bf8975e9c3dd2191c927e601f41e6
Margarita Novokh <r28611@gmail.com>
2021-12-20 23:25:10 +0100

    added tableView

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUi()
+        tableView.dataSource = self
     }
-
-
+    
+    private func setupUi() {
+        view.addSubview(tableView)
+        let margins = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: margins.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        ])
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+    
+}
